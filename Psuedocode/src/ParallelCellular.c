2d545b5424ca5c1bf38baf8d96bd153a73873f59
FinityM <71716915+FinityM@users.noreply.github.com>
2021-10-15 10:18:00 +0100

    Created C code to test allocation of data to a 2d array

--- Psuedocode/src/ParallelCellular.c
+++ Psuedocode/src/ParallelCellular.c
@@ -2,18 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct table 
+typdef struct table
 {
     int rows;
     int cols;
     int **data;
-
 } table;
 
 void *maxthread(void *arg)
 {
     table t = *((table *)arg);
-    for (int i = 0; i < t.rows; i++)
+    for(int i = 0; i < t.rows; i++)
     {
         for(int j = 0; j < t.cols; j++)
         {
@@ -35,28 +34,19 @@ int main()
     t.cols = 10;
     t.data = malloc(sizeof(int *) * t.rows);
 
-    for (int i = 0; i < t.rows; i++) 
+    for(int i = 0; i < t.rows; i++)
     {
-        t.data[i] = malloc(sizeof(int) * t.cols);
-
-        for (int j = 0; j < t.cols; j++)
+        t.data[i] = mallod(sizeof(int) * t.cols);
+        for(int j = 0; j < t.cols; j++)
         {
             t.data[i][j] = i * j;
         }
     }
 
-    for (int i = 0; i < t.rows; i++)
+    for(int i = 0; i < t.rows; i++)
     {
         free(t.data[i]);
     }
-
-    free(t.data);
+    free(t.data[i]);
     return 0;
-
-
 }
-
-
-
-
-
